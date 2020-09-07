bb206e2921d16d33d5182d5f74e053b6f07c8a23
Simon R <simon@reinisch.io>
2020-09-07 17:17:50 +0200

    Update Header and base styles

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,11 +4,11 @@ import {FunctionalComponent, h} from 'preact';
 import styles from './Header.module.scss';
 
 export const Header: FunctionalComponent = () => (
-    <div className={styles.header}>
+    <header className={styles.header} id="home">
         <h1>{config.name}</h1>
         <LettersSpinner interval={2000}
                         speed={350}
                         words={config.slogans}
                         className={styles.spinner}/>
-    </div>
+    </header>
 );
