796e761cca04b3ded875a2f5f15ba418b66abfa8
Simon R <simon@reinisch.io>
2020-09-17 16:19:38 +0200

    Make Footer links accessible

--- src/app/components/Link.tsx
+++ src/app/components/Link.tsx
@@ -1,8 +1,11 @@
+import {cn} from '@utils/preact-utils';
 import {h, RenderableProps} from 'preact';
 import {forwardRef} from 'preact/compat';
+import styles from './Link.module.scss';
 
 type Props = {
     className?: string;
+    label?: string;
     href: string;
 };
 
@@ -16,14 +19,22 @@ export const Link = forwardRef<HTMLAnchorElement, RenderableProps<Props>>((props
         e.preventDefault();
     };
 
+    if (env.NODE_ENV === 'development') {
+        /* eslint-disable no-console */
+        if (!props.children && !props.label) {
+            console.warn(`Link should have a label associated with it. Link: ${props.href}`);
+        }
+    }
+
     return (
-        <a className={props.className}
+        <a className={cn(props.className, styles.link)}
            data-cursor-focus={true}
            rel="noopener noreferrer"
            target="_blank"
            ref={ref}
            href={props.href}
            onClick={props.href.startsWith('#') ? handleHashLink : undefined}>
+            {props.label && <p className={styles.visibleHidden}>{props.label}</p>}
             {props.children}
         </a>
     );
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,5 +1,10 @@
+/* eslint-disable no-unused-vars */
 declare module '*.scss';
 declare module '*.svg';
 declare module '*.png';
 declare module '*.jpg';
 declare module '*.webp';
+
+declare let env: {
+    NODE_ENV: 'production' | 'development'
+};
