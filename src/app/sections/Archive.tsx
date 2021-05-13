e8378c9a13c83bc8b67232f9201aa80a8f14dc7e
Simon R <simon@reinisch.io>
2021-05-13 10:49:38 +0200

    Mark more projects as archived

--- src/app/sections/Archive.tsx
+++ src/app/sections/Archive.tsx
@@ -9,7 +9,7 @@ export const Archive: FunctionalComponent = () => {
     return (
         <PageSection id="archive" title="Archive"
                      intro="Things I once worked on. They're not up-to-date, may not work anymore but represent an important part of my journey.">
-            <Projects projects={config.projects.legacy}
+            <Projects projects={config.projects.archive}
                       mobileLimitBase={3}/>
             <p className={styles.bottomText}>...any <Link href="https://github.com/Simonwep?tab=repositories">many more</Link>.</p>
         </PageSection>
--- src/config.ts
+++ src/config.ts
@@ -1,19 +1,19 @@
 import rawConfig from '../config/config.json';
 
-export type Project = {
+export interface Project {
     title: string;
     description: string;
     tags: string[];
     link: string;
     created: string;
-};
+}
 
-export type Config = {
+export interface Config {
     name: string;
     projects: {
         current: Project[];
-        legacy: Project[];
+        archive: Project[];
     };
-};
+}
 
 export const config = rawConfig as Config;
